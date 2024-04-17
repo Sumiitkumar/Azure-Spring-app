68f7bcc41537c2331ff50aaa1f59c7784c532d1f
Sumit Kumar <124031868+Sumiitkumar@users.noreply.github.com>
2024-04-18 01:12:30 +0530

    Update TestController.java

--- src/main/java/com/springazure/springbootazure/controller/TestController.java
+++ src/main/java/com/springazure/springbootazure/controller/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
     @GetMapping
     public Object hello() {
         Map<String, String> object = new HashMap<>();
-        object.put("name", "Azure WEB APPS");
+        object.put("name", "Azure Web APPS");
         object.put("email", "sumiitkumar@gmail.com");
         return object;
     }
